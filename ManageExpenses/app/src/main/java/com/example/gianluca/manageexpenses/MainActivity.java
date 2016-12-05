81bab1aa3d6c8202b7246e5d30f2d272f3023e16
Gianluca Monica <gianluca.monica@studenti.unipr.it>
2016-12-05 18:44:30 +0100

    Layout Initiated

--- ManageExpenses/app/src/main/java/com/example/gianluca/manageexpenses/MainActivity.java
+++ ManageExpenses/app/src/main/java/com/example/gianluca/manageexpenses/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gianluca.manageexpenses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editName = (EditText) findViewById(R.id.editName);
+        EditText editSum = (EditText) findViewById(R.id.editSum);
+
+        Button addExpense = (Button) findViewById(R.id.addExpense);
+
+
     }
 }
